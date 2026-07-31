69f942aea772fa17137ff5b45061fef84cf291ba
mehul0948 <mehulkumar95820@gmail.com>
2026-07-31 17:49:55 +0530

    Day 1 DSA - Patterns

---
+++ Patterns.java
@@ -0,0 +1,99 @@
+// Day 1 or DSA >> Patterns
+
+// Pattern no 1
+
+/*
+public class Patterns {
+    public static void main(String[] args) {
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
+//                                               >>             Output                <<
+
+
+****
+****
+****
+****
+
+*/
+
+// Pattern no 2
+
+/*
+public class Patterns {
+
+    public static void main(String[] args) {
+        for(int i=0;i<4;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}
+
+//                                               >>             Output                <<
+
+
+*
+**
+***
+****
+
+*/
+
+// Pattern no 3
+
+/*
+public class Patterns {
+
+     public static void main(String[] args) {
+        for(int i=1;i<5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+
+//                                               >>             Output                <<
+
+
+1
+12
+123
+1234
+
+*/
+
+// Pattern no 4
+/*
+
+public class Patterns {
+    public static void main(String[] args) {
+        for(int i=1;i<5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
+
+//                                               >>             Output                <<
+
+1
+22
+333
+4444
+
+*/
